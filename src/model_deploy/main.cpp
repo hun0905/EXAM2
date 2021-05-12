dfab2865b7acc02ec82fe2eebcd4cef0eb12c216
hun0905 <a0905121101@gmail.com>
2021-05-12 01:47:38 -0700

    04:47

--- src/model_deploy/main.cpp
+++ src/model_deploy/main.cpp
@@ -18,176 +18,107 @@
 #include "mbed_rpc.h"
 #include "uLCD_4DGL.h"
 #include <math.h>  
+#define PI 3.14159265
 void gesture();
 void gesture_UI();
 int PredictGesture(float* output) ;
-void acc_capture(Arguments *in, Reply *out);
+void acc(Arguments *in, Reply *out);
 Thread t_g;
 EventQueue queue_g(32 * EVENTS_EVENT_SIZE);
 
 InterruptIn btnRecord(USER_BUTTON);
 EventQueue queue(32 * EVENTS_EVENT_SIZE);
+BufferedSerial pc(USBTX, USBRX);
 Thread t;
 
-int16_t pDataXYZ[3] = {0};
+int16_t pData[3] = {0};
 int idR[32] = {0};
 int indexR = 0;
+int times = 0;
+RPCFunction rpc_acc(&acc, "acc");
+int gesture_ID = 0;
+int data[20] ;
+
+void messageArrived(MQTT::MessageData& md) {
+    MQTT::Message &message = md.message;
+    char msg[300];
+    sprintf(msg, "Message arrived: QoS%d, retained %d, dup %d, packetID %d\r\n", message.qos, message.retained, message.dup, message.id);
+    printf(msg);
+    ThisThread::sleep_for(1000ms);
+    char payload[300];
+    sprintf(payload, "Payload %.*s\r\n", message.payloadlen, (char*)message.payload);
+    printf(payload);
+    ++arrivedcount;
+}
+
+void publish_message(MQTT::Client<MQTTNetwork, Countdown>* client) {
+    if(mode_p)
+    {
+      MQTT::Message message;
+      char buff[100];
+      sprintf(buff, "%d", gesture_ID);
+      message.qos = MQTT::QOS0;
+      message.retained = false;
+      message.dup = false;
+      message.payload = (void*) buff;
+      message.payloadlen = strlen(buff) + 1;
+      int rc = client->publish(topic, message);
+  
+      printf("rc:  %d\r\n", rc);
+      printf("Puslish message: %s\r\n", buff);
+      mode_p = false;
+    }
+}
+
+void close_mqtt() {
+    closed = true;
+}
 
 void record(void) {
-   BSP_ACCELERO_AccGetXYZ(pDataXYZ);
-   printf("%d, %d, %d\n", pDataXYZ[0], pDataXYZ[1], pDataXYZ[2]);
+   BSP_ACCELERO_AccGetXYZ(pData);
+   printf("%d, %d, %d\n", pData[0], pData[1], pData[2]);
+   double deg = (double)pData[2]/sqrt(pData[0]*pData[0]+pData[1]*pData[1]+pData[2]*pData[2]);
+    if(deg < cos(30* PI/ 180.0))
+        times++;
+    else
+        times = 0;
+    if(times >= 10)
+    {
+        data[gesture_ID]++;
+        times = 0;
+    }
 }
 
 void startRecord(void) {
    printf("---start---\n");
+   times = 0;
+   data[gesture_ID] = 0;
    idR[indexR++] = queue.call_every(1ms, record);
    indexR = indexR % 32;
 }
 
 void stopRecord(void) {
-   printf("---stop---\n");
-   for (auto &i : idR)
+    printf("case of change direction %d\n", data[gesture_ID]);
+    printf("---stop---\n");
+    gesture_ID++;
+    for (auto &i : idR)
       queue.cancel(i);
 }
-void (Arguments *in, Reply *out)
+void acc(Arguments *in, Reply *out)
 {
-   printf("Start accelerometer init\n");
+   
    BSP_ACCELERO_Init();
+   printf("Start accelerometer init\n");
    t.start(callback(&queue, &EventQueue::dispatch_forever));
    btnRecord.fall(queue.event(startRecord));
    btnRecord.rise(queue.event(stopRecord));
 }
-void gesture_UI(Arguments *in, Reply *out){
-  bool success = true;
-  t_g.start(callback(&queue_g, &EventQueue::dispatch_forever));
-  queue_g.call(&gesture);
-  // Have code here to call another RPC function to wake up specific led or close it.
-  char buffer[200], outbuf[256];
-  char strings[20];
-}
 
 
-int PredictGesture(float* output) {
-    // How many times the most recent gesture has been matched in a row
-    static int continuous_count = 0;
-    // The result of the last prediction
-    static int last_predict = -1;     
-    // Find whichever output has a probability > 0.8 (they sum to 1)
-    int this_predict = -1;
-    for (int i = 0; i < label_num; i++) {
-      if (output[i] > 0.8) this_predict = i;
-    }     
-    // No gesture was detected above the threshold
-    if (this_predict == -1) {
-      continuous_count = 0;
-      last_predict = label_num;
-      return label_num;
-    }     
-    if (last_predict == this_predict) {
-      continuous_count += 1;
-    } else {
-      continuous_count = 0;
-    }
-    last_predict = this_predict;      
-    // If we haven't yet had enough consecutive matches for this gesture,
-    // report a negative result
-    if (continuous_count < config.consecutiveInferenceThresholds[this_predict]) {
-      return label_num;
-    }
-    // Otherwise, we've seen a positive result, so clear all our variables
-    // and report it
-    continuous_count = 0;
-    last_predict = -1;      
-    return this_predict;
-}
 
-void gesture() 
-{
 
-  // Whether we should clear the buffer next time we fetch data
-  bool should_clear_buffer = false;
-  bool got_data = false;
-  // The gesture index of the prediction
-  int gesture_index;
-  // Set up logging.
-  static tflite::MicroErrorReporter micro_error_reporter;
-  tflite::ErrorReporter* error_reporter = &micro_error_reporter;
-  // Map the model into a usable data structure. This doesn't involve any
-  // copying or parsing, it's a very lightweight operation.
-  const tflite::Model* model = tflite::GetModel(g_magic_wand_model_data);
-  if (model->version() != TFLITE_SCHEMA_VERSION) {
-    error_reporter->Report(
-        "Model provided is schema version %d not equal "
-        "to supported version %d.",
-        model->version(), TFLITE_SCHEMA_VERSION);
-    
-  }
-  // Pull in only the operation implementations we need.
-  // This relies on a complete list of all the ops needed by this graph.
-  // An easier approach is to just use the AllOpsResolver, but this will
-  // incur some penalty in code space for op implementations that are not
-  // needed by this graph.
-  static tflite::MicroOpResolver<6> micro_op_resolver;
-  micro_op_resolver.AddBuiltin(
-      tflite::BuiltinOperator_DEPTHWISE_CONV_2D,
-      tflite::ops::micro::Register_DEPTHWISE_CONV_2D());
-  micro_op_resolver.AddBuiltin(tflite::BuiltinOperator_MAX_POOL_2D,
-                               tflite::ops::micro::Register_MAX_POOL_2D());
-  micro_op_resolver.AddBuiltin(tflite::BuiltinOperator_CONV_2D,
-                               tflite::ops::micro::Register_CONV_2D());
-  micro_op_resolver.AddBuiltin(tflite::BuiltinOperator_FULLY_CONNECTED,
-                               tflite::ops::micro::Register_FULLY_CONNECTED());
-  micro_op_resolver.AddBuiltin(tflite::BuiltinOperator_SOFTMAX,
-                               tflite::ops::micro::Register_SOFTMAX());
-  micro_op_resolver.AddBuiltin(tflite::BuiltinOperator_RESHAPE,
-                               tflite::ops::micro::Register_RESHAPE(), 1);
-  // Build an interpreter to run the model with
-  static tflite::MicroInterpreter static_interpreter(
-      model, micro_op_resolver, tensor_arena, kTensorArenaSize, error_reporter);
-  tflite::MicroInterpreter* interpreter = &static_interpreter;
-  // Allocate memory from the tensor_arena for the model's tensors
-  interpreter->AllocateTensors();
-  // Obtain pointer to the model's input tensor
-  TfLiteTensor* model_input = interpreter->input(0);
-  if ((model_input->dims->size != 4) || (model_input->dims->data[0] != 1) ||
-      (model_input->dims->data[1] != config.seq_length) ||
-      (model_input->dims->data[2] != kChannelNumber) ||
-      (model_input->type != kTfLiteFloat32)) {
-    error_reporter->Report("Bad input tensor parameters in model");
-    
-  }
-  int input_length = model_input->bytes / sizeof(float);
-  TfLiteStatus setup_status = SetupAccelerometer(error_reporter);
-  if (setup_status != kTfLiteOk) {
-    error_reporter->Report("Set up failed\n"); 
-  }
-  error_reporter->Report("Set up successful...\n");
-  while (tilt_m == false) {
-    // Attempt to read new data from the accelerometer
-    got_data = ReadAccelerometer(error_reporter, model_input->data.f,
-                                 input_length, should_clear_buffer);
-    // If there was no new data,
-    // don't try to clear the buffer again and wait until next time
-    if (!got_data) {
-      should_clear_buffer = false;
-      continue;
-    }
-    // Run inference, and report any error
-    TfLiteStatus invoke_status = interpreter->Invoke();
-    if (invoke_status != kTfLiteOk) {
-      error_reporter->Report("Invoke failed on index: %d\n", begin_index);
-      continue;
-    }
-    // Analyze the results to obtain a prediction
-    gesture_index = PredictGesture(interpreter->output(0)->data.f);
-    // Clear the buffer next time we read data
-    should_clear_buffer = gesture_index < label_num;
-    // Produce an output
-    if (gesture_index < label_num) {
-        error_reporter->Report(config.output_message[gesture_index]);
-    }
-  }
-}
+
+
 
 
 
@@ -195,7 +126,7 @@ int main() {
   char buf[256], outbuf[256];
   FILE *devin = fdopen(&pc, "r");
   FILE *devout = fdopen(&pc, "w");
-  /*
+  
   wifi = WiFiInterface::get_default_instance();
   if (!wifi) {
           printf("ERROR: No WiFiInterface found.\r\n");
@@ -233,10 +164,9 @@ int main() {
   t_mode.start(callback(&mode_queue, &EventQueue::dispatch_forever));
   mqtt_thread.start(callback(&mqtt_queue, &EventQueue::dispatch_forever));
   
-  btn2.rise(mode_queue.event(&mode));
-  //btn3.rise(&close_mqtt);
-  mqtt_queue.call_every(500ms,&publish_message, &client);
-  */
+  btnRecord.fall(mqtt_queue.event(&publish_message, &client));
+  
+  
   while(1) {
     memset(buf, 0, 256);      // clear buffer
     for(int i=0; ; i++) {
